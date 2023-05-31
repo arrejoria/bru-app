83110ee3f0dbc849bdc5deb209674f08d85e3f7e
Lucas Exequiel <arrejoria1994@gmail.com>
2023-05-31 19:01:08 -0300

    Bru update [screen loading, scripts]

--- js/main.js
+++ js/main.js
@@ -27,10 +27,15 @@ const brubankinteress = {
 const { clasicoPesos, data } = brubankinteress;
 
 const fechaHoy = new Date();
-const opcionesFecha = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const fechaFormateada = fechaHoy.toLocaleDateString('es-ES', opcionesFecha);
+const opcionesFecha = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+const fechaFormateada = fechaHoy.toLocaleDateString("es-ES", opcionesFecha);
 
-console.log(fechaFormateada); 
+// console.log(fechaFormateada);
 // Escuchar el evento change en el select timePeriod
 const timePeriod = document.querySelector('select[name="timePeriod"]');
 const montoEl = document.querySelector('input[name="amount"]');
@@ -38,21 +43,24 @@ const montoEl = document.querySelector('input[name="amount"]');
 let periodId = 0;
 let monto = 0;
 
-document.addEventListener('DOMContentLoaded', calcularIntereses(Number(montoEl.value), Number(timePeriod.value)))
+document.addEventListener(
+  "DOMContentLoaded",
+  calcularIntereses(Number(montoEl.value), Number(timePeriod.value))
+);
 montoEl.addEventListener("input", (e) => {
   handleAmount();
 });
 
 timePeriod.addEventListener("change", () => {
-  console.dir("change");
+  // console.dir("change");
   handleTimePeriod();
 });
 
 function handleAmount() {
   monto = Number(montoEl.value);
   periodId = Number(timePeriod.value);
-  console.log("handle amount", typeof monto, monto);
-    calcularIntereses(monto, periodId);
+  // console.log("handle amount", typeof monto, monto);
+  calcularIntereses(monto, periodId);
 }
 
 function handleTimePeriod() {
@@ -68,23 +76,131 @@ function calcularIntereses(monto, periodId) {
   const intereses = monto * interes * (plazo / 365);
   const interesesGanados = monto + intereses;
 
-  console.log("plazo:", plazo, "monto:", monto, "interes:", interes);
-  data.montoInicial = '$ ' + monto.toLocaleString('es-AR');
-  data.interesesGanados = '$ ' + interesesGanados.toLocaleString('es-AR');
-  data.netoACobrar = '$ ' + intereses.toLocaleString('es-AR');
-  data.periodoRenovacion = plazo === 30 ? plazo / 30 + ' mes' : plazo / 30 + ' meses'
-  data.tna = interes * 100 + '%'
-  data.tea = clasicoPesos.tea[periodId] + '%'
-  console.log(data);
+  // console.log("plazo:", plazo, "monto:", monto, "interes:", interes);
+  data.montoInicial = "$ " + monto.toLocaleString("es-AR");
+  data.interesesGanados = "$ " + interesesGanados.toLocaleString("es-AR");
+  data.netoACobrar = "$ " + intereses.toLocaleString("es-AR");
+  data.periodoRenovacion =
+    plazo === 30 ? plazo / 30 + " mes" : plazo / 30 + " meses";
+  data.tna = interes * 100 + "%";
+  data.tea = clasicoPesos.tea[periodId] + "%";
+  // console.log(data);
 
   handleResults(data);
 }
 
 function handleResults(data) {
-    const resultList = document.querySelectorAll(".result__item");
-    console.log(data)
-      Object.entries(data).forEach(([clave, valor], index) => {
-        console.log(clave, valor, index)
-        resultList[index].children[1].textContent = valor
-    });
+  const resultList = document.querySelectorAll(".result__item");
+  // console.log(data)
+  Object.entries(data).forEach(([clave, valor], index) => {
+    // console.log(clave, valor, index)
+    resultList[index].children[1].textContent = valor;
+  });
+}
+
+var detects = {
+  hasjquery: "jQuery" in window,
+  itstuesday: function () {
+    var d = new Date();
+    return d.getDay() === 2;
+  },
+};
+Modernizr.addTest(detects);
+
+const screenLoading = document.querySelector(".screen__loading");
+const screenWords = document.querySelectorAll(".animated-words");
+let time = 4;
+
+screenWords.forEach((word) => {
+  word.style.setProperty("--scren-time", time + "s");
+  word.style.setProperty("--load-words", "100%");
+});
+
+// Carga animacion de screen lette
+setTimeout(() => {
+  screenWords[0].classList.add("screen-text-animation");
+  screenWords[1].classList.add("screen-text-animation");
+}, (time - 1) * 1000);
+
+// Carga screen laoding animation 4 seg posterior a la animacion de screen words
+setTimeout(function () {
+  screenLoading.classList.add("screen-animation");
+
+  // Oculta screen laoding 1.5 seg posterior a la animacion
+  setTimeout(() => {
+    screenLoading.style.display = "none";
+  }, 1000); // Ajusta el tiempo de espera para que coincida con la duración de la animación (1.5 segundos en este caso)
+}, time * 1000);
+
+try {
+  console.log("trycatch");
+} catch (error) {
+  console.error(error);
+}
+
+const inpTest = document.getElementById("inpTest");
+
+inpTest.addEventListener("change", (e) => {
+  let value = "";
+  value = e.target.value;
+  const formattedValue = formatNumber(value);
+  inpTest.value = formattedValue;
+});
+
+function formatNumber(value) {
+  const number = parseInt(value);
+  console.log(number);
+  if (isNaN(number)) {
+    return "";
   }
+  return number.toLocaleString("es-AR", { style: "decimal", currency: "ARS" });
+}
+
+const numberParser = (value) =>
+  parseFloat(value.replace(".", "").replace(",", "."));
+
+const numberFormatter = (value) => {
+  if (value == null || value === "") {
+    return value;
+  }
+  return value?.toLocaleString();
+};
+
+const numero = 100100;
+console.log(numero.indexOf())
+const result = numberFormatter(num);
+const result2 = numberParser(result);
+console.log(result);
+console.log(result2);
+
+function parseText(num) {
+  num = num.toString();
+  switch (num.length) {
+    case 8:
+      console.log(num, "long. 8");
+      break;
+    case 7:
+      console.log(num, "long. 7 con punto");
+      break;
+    case 6:
+      num.splice('3','', '.')
+      console.log(num, "long. 6");
+      break;
+    case 5:
+      console.log(num, "long. 5");
+      break;
+    case 4:
+      console.log(num, "long. 4");
+      break;
+    default:
+      console.log('default case, la longitud es < 5 o > a 10')
+  }
+  console.log(num, typeof num);
+}
+
+function parseTextDot(num, length){
+  num = num.toString()
+  let numLength = num.length
+
+}
+parseText(100100);
