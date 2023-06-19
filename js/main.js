3a286fcc9d2b4b471be9af4818756600c0a710cc
Lucas Exequiel <arrejoria1994@gmail.com>
2023-06-19 14:29:34 -0300

    Bru update [HTML, Scripts]

--- js/main.js
+++ js/main.js
@@ -12,10 +12,8 @@ const localeObj = {
 
 const bruSubmit = document.getElementById("bruSubmit");
 const montoInp = document.querySelector('input[name="amount"]');
-const timePeriod = document.querySelector('select[name="timePeriod"]');
-const resultItems = document.querySelectorAll(".result__item");
 
-// const getPeriod = timePeriod?Array.from(timePeriod.children).slice(1).map( (el) => {
+// const getPeriod = selectMeses?Array.from(selectMeses.children).slice(1).map( (el) => {
 //    return el.dataset.placeholder
 // }) : null
 
@@ -24,12 +22,14 @@ let monto, periodId;
 // Handling Events
 bruSubmit.addEventListener("click", () => {
   console.log("Calculando intereses ganados");
-  storageResults();
+  storageAll();
   showResults();
+  loadPlazo()
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  storageResults();
+  setSelectData();
+  storageAll();
 });
 
 montoInp.addEventListener("input", (e) => {
@@ -39,7 +39,10 @@ montoInp.addEventListener("input", (e) => {
 
 // BruApp functions
 
-
+/**
+ *
+ * Amount Section Starts here
+ */
 function formatAmount(num) {
   removeError(".error-message");
 
@@ -101,15 +104,47 @@ function removeError(elements) {
   });
   bruSubmit.disabled = false;
 }
+/**
+ *
+ * Asignar data attribute de options al select de meses
+ */
+const selectMeses = document.querySelector('select[name="selectMeses"]');
+const resultItems = document.querySelectorAll(".result__item");
+selectMeses.addEventListener("change", setSelectData());
+
+function setSelectData() {
+  const selectedOption = selectMeses.options[selectMeses.selectedIndex];
+  // console.log(selectedOption);
+  const monthId = selectedOption.getAttribute("data-month-id");
+  selectMeses.setAttribute("data-month-id", monthId);
+  // console.log(selectMeses);
+}
+/**
+ *
+ * Storage Section Starts here
+ */
+function storageAll() {
+  const plazoData = {
+    clasicoPesos: {
+      dias: [],
+      tna: [0.92, 0.92, 0.94, 0.97, 1.0],
+      tea: [142.7, 135.5, 132.9, 120.9, 100],
+    },
+  };
+  plazoData.clasicoPesos.dias = setDias();
+  plazoData.plazoResults = setPlazoResults();
+  setPlazos()
+  storageData(plazoData);
+  showResults() 
+}
 
 function storageData(obj) {
   if (obj) {
     for (let key in obj) {
       // console.log("inside stdata bf owp", obj, "key:", key);
-      // console.log("obj prop:", obj);
-      if (obj.hasOwnProperty(key)) {
-        // console.log("inside stdata", obj);
-
+      console.log("obj prop:", obj[key]);
+      if (obj.hasOwnProperty(key) && obj[key] !== null) {
+        console.log("inside stdata", obj);
         try {
           const objString = JSON.stringify(obj[key]); // Almacena el objeto en una variable separada
           localStorage.setItem(`${key}`, objString);
@@ -121,77 +156,149 @@ function storageData(obj) {
   }
 }
 
-function storageResults() {
-  const plazoData = {
-    clasicoPesos: {
-      dias: [30, 60, 90, 180, 360],
-      tna: [0.59, 0.92, 0.94, 0.97, 1.0],
-      tea: [142.7, 135.5, 132.9, 120.9, 100],
-    },
-  };
+function showResults() {
 
-  plazoData.plazoResults = storePlazoInfo();
-  storageData(plazoData);
-  showResults()
-}
+   const results = JSON.parse(localStorage.getItem("plazoResults"));
+   if(results !== null)
+    console.log('inside set plazo results')
+   Object.entries(results).forEach(([clave, valor], index) => {
+     if (Object.hasOwnProperty.call(results, clave)) {
+       if (clave == "periodoRenovacion") valor = valor + " días";
+       return (resultItems[index].children[1].textContent = valor);
+     }
+   });
 
-function storePlazoInfo() {
+ }
+ 
+function setPlazoResults() {
   try {
     const monto = parseFloat(montoInp.value.replace(/\./g, ""));
-    const periodId = timePeriod.value;
-
+    const periodId = parseInt(selectMeses.getAttribute("data-month-id"));
     const bruInterests = JSON.parse(localStorage.getItem("clasicoPesos"));
+    console.log('bruInterests::',bruInterests)
     if (bruInterests) {
       console.log(bruInterests);
       const { dias, tna, tea } = bruInterests;
-      const intereses = monto * tna[periodId] * (dias[periodId] / 365);
-      const fechaActual = new Date();
-      const fechaFormateada = fechaActual.toLocaleDateString("es-ES", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      });
+      const intereses = monto * tna[periodId] * (dias / 365);
 
       const storedInfo = {
         montoInicial: monto.toLocaleString("es-AR", localeObj),
-        interesesGanados: (monto + intereses).toLocaleString("es-AR", localeObj),
+        interesesGanados: (monto + intereses).toLocaleString(
+          "es-AR",
+          localeObj
+        ),
         netoACobrar: intereses.toLocaleString("es-AR", localeObj),
-        periodoRenovacion: dias[periodId] + ' días',
+        periodoRenovacion: dias,
         tna: tna[periodId],
         tea: tea[periodId],
-        // fechaInicio: fechaFormateada,
       };
-
+      console.log('storedInfo >>>>>>',storedInfo)
       return storedInfo;
     }
   } catch (error) {
     console.error("Error al almacenar información del plazo fijo:", error);
   }
-
-  return null;
+  return null
 }
 
-function storagePlazos() {
-  const { montoInicial, interesesGanados, periodoRenovacion, fechaInicio } =
-    plazoData;
+function setPlazos() {
+  const objPlazos = JSON.parse(localStorage.getItem("objPlazos")) || {};
 
-  objPlazos = {
+  const plazos = {
     monto: montoInicial,
     intereses: interesesGanados,
     periodo: periodoRenovacion,
-    inicio: fechaInicio,
-    finaliza: fechaInicio + 31,
+    fechas: plazoFechas(),
   };
+
+  var clavePlazo = 'plazo'; // Reemplaza 'claveEjemplo' con la clave que desees utilizar
+
+  if (objPlazos.hasOwnProperty(clavePlazo)) {
+    // Si la clave existe, agrega el objeto plazos al arreglo existente
+    objPlazos[clavePlazo].push(plazos);
+  } else {
+    // Si la clave no existe, crea un nuevo arreglo con plazos como único elemento
+    objPlazos[clavePlazo] = [plazos];
+  }
+
+  // Guardar el objeto objPlazos actualizado en el localStorage
+  localStorage.setItem("objPlazos", JSON.stringify(objPlazos));
+  console.log(plazos)
+
+  return plazos;
 }
 
-function showResults(){
-  const results = JSON.parse(localStorage.getItem('plazoResults'));
+// Manejar fecha de inicio y final de un plazo fijo y cantidad de dias entre ambas
+function plazoFechas() {
+  try {
+    const fechaActual = new Date();
+    const fechaFinal = new Date();
+    const mesCantidad = parseInt(selectMeses.value);
+    const mesActual = fechaActual.getMonth();
 
-  Object.entries(results).forEach(([clave, valor], index) => {
-    if (Object.hasOwnProperty.call(results, clave)) {
-      return (resultItems[index].children[1].textContent = valor);
-    }
-  });
+    fechaFinal.setMonth(mesActual + mesCantidad);
+    fechaFinal.toLocaleDateString("es-ES", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    });
+
+    const actualFormateada = fechaActual.toLocaleDateString("es-ES", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    });
+    const finalFormateada = fechaFinal.toLocaleDateString("es-ES", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    });
+
+    const fechasFormateadas = [actualFormateada, finalFormateada];
+    return fechasFormateadas;
+  } catch (error) {
+    console.error("Error al manejar las fechas de los plazos:", error);
+  }
+}
+
+function setDias() {
+  try {
+    const mesCantidad = parseInt(selectMeses.value);
+    const fechaInicio = new Date();
+    const fechaFinal = new Date();
+    fechaFinal.setMonth(fechaFinal.getMonth() + mesCantidad);
+
+    const diferenciaMs = fechaFinal.getTime() - fechaInicio.getTime();
+    const diferenciaDias = diferenciaMs / (24 * 60 * 60 * 1000);
+    return diferenciaDias;
+  } catch (error) {
+    console.error("Error al obtener la cantidad de días de los plazos.", error);
+  }
+}
+
+/**
+* Generar plazos fijos y almacenarlos en storage
+*/
+bruSubmit.addEventListener('click', () => {
+
+})
+
+function loadPlazo(){
+  const getPlazos = JSON.parse(localStorage.getItem('objPlazos'));
+  for(const plazo in getPlazos){
+    console.log(getPlazos)
+  }
+
+
+  const plazoElement = `
+  <tr>
+  <td>$107.561,644</td>
+  <td>$7.561,644</td>
+  <td>1 Mes</td>
+  <td>11 JUL 2023</td>
+  <td>14 AGO 2023</td>
+  <td><button>agregar</button></td>
+</tr>`
 }
 /**
  * Screen Loading Scripts starts here
